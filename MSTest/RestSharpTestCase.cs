df3e82dc57f3d0db4a455e94f81b47785e1e4b9d
Chetan2028 <bmchetan2028@gmail.com>
2020-12-01 19:40:16 +0530

    [Chetan] Add : Added test cases to add employee

--- MSTest/RestSharpTestCase.cs
+++ MSTest/RestSharpTestCase.cs
@@ -5,6 +5,7 @@ using System.Net;
 using EmployeePayroll_RestAPI;
 using Newtonsoft.Json;
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace MSTest
 {
@@ -38,6 +39,7 @@ namespace MSTest
         }
 
         /// <summary>
+        /// T.C -> 1
         /// Retrieves the employee list
         /// </summary>
         [TestMethod]
@@ -48,13 +50,36 @@ namespace MSTest
             //Assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
             List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            int dataCount = 4;
+            int dataCount = 5;
             Assert.AreEqual(dataCount, dataResponse.Count);
 
             foreach (Employee emp in dataResponse)
             {
                 Console.WriteLine("id : " + emp.id + " name : " + emp.name + " salary : " + emp.salary);
             }
+        }
+
+        [TestMethod]
+        public void GivenEmployee_OnPost_ShouldReturnAddEmployee()
+        {
+            //Arrange
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jObjectBody = new JObject();
+            jObjectBody.Add("name", "SRK");
+            jObjectBody.Add("salary", "78526");
+            //id not added but addded in json file
+
+            request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("SRK", dataResponse.name);
+            Assert.AreEqual("78526", dataResponse.salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
