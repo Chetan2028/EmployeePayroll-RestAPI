17c70c706e75266797c88d55fa915d02601a895b
Chetan2028 <bmchetan2028@gmail.com>
2020-12-01 18:41:18 +0530

    [Chetan] Add : Added Test Cases to retrieve Employee Data and Count

---
+++ MSTest/RestSharpTestCase.cs
@@ -0,0 +1,55 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+using EmployeePayroll_RestAPI;
+using Newtonsoft.Json;
+
+namespace MSTest
+{
+    [TestClass]
+    public class RestSharpTestCase
+    {
+        RestClient client;
+
+        /// <summary>
+        /// Setups this instance.
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            //This path is used by all api's and urls to access Json Rest Api's
+            client = new RestClient("http://localhost:4000");
+        }
+
+        /// <summary>
+        /// Gets the employee list.
+        /// </summary>
+        /// <returns></returns>
+        private IRestResponse GetEmployeeList()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/employees", Method.GET);
+
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+
+        /// <summary>
+        /// Retrieves the employee list
+        /// </summary>
+        [TestMethod]
+        public void OnCallingList_ReturnEmployeeList()
+        {
+            IRestResponse response = GetEmployeeList();
+
+            //Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            int dataCount = 4;
+            Assert.AreEqual(dataCount, dataResponse.Count);
+        }
+    }
+}
+
